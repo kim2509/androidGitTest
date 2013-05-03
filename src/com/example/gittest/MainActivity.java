22e6af06b3a3c378304fa26870a20c80947f8b7e
김대용 <kim2509@gmail.com>
2013-05-03 13:59:23 +0900

    hhh

--- src/com/example/gittest/MainActivity.java
+++ src/com/example/gittest/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.gittest;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		WebView myWebView = (WebView) findViewById(R.id.webview);
+		myWebView.loadUrl("http://www.gmarket.co.kr");
+		WebSettings webSettings = myWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		myWebView.setWebViewClient(new WebViewClient());
 	}
 
 	@Override
