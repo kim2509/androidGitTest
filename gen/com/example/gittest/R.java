f7e61cf01caa29eff92eddb59dd85105f9eaef13
김대용 <kim2509@gmail.com>
2013-05-07 13:29:41 +0900

    added a webview

--- gen/com/example/gittest/R.java
+++ gen/com/example/gittest/R.java
@@ -19,24 +19,32 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int list_padding=0x7f040003;
+        public static final int shadow_width=0x7f040004;
+        public static final int slidingmenu_offset=0x7f040002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int webview=0x7f080000;
+        public static final int action_settings=0x7f090002;
+        public static final int checkBox1=0x7f090001;
+        public static final int webview=0x7f090000;
+    }
+    public static final class integer {
+        public static final int num_cols=0x7f050000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int menu=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -60,10 +68,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
