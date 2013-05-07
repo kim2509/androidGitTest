81527eb924b4ceaf97f983e95169c60828ee7bd0
김대용 <kim2509@gmail.com>
2013-05-07 13:15:13 +0900

    add a file

---
+++ src/com/example/gittest/MainActivity2.java
@@ -0,0 +1,32 @@
+package com.example.gittest;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity2 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		WebView myWebView = (WebView) findViewById(R.id.webview);
+		//myWebView.loadUrl("http://www.gmarket.co.kr");
+		myWebView.loadUrl("http://www.gmarket.co.kr");
+		WebSettings webSettings = myWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		myWebView.setWebViewClient(new WebViewClient());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
